355e2b175a0d8e3239a108dee96cf312496f2443
Sergi <sergisc78@gmail.com>
2021-04-19 14:00:22 +0200

    19 abril_2

--- views/login.php
+++ views/login.php
@@ -7,36 +7,56 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
 
-    <!-- Compiled and minified CSS 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">-->
+    <!-- MATERIALIZE -->
 
-    <!-- Materialize icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- MATERIALIZE ICONS -->
 
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
-    <!-- Google fonts-->
+    <!-- GOOGLE FONTS-->
 
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Montserrat+Alternates:wght@100;300;600&display=swap" rel="stylesheet">
 
-    <!-- css file -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
+
+    <!-- CSS FILE -->
+
     <link rel="stylesheet" href="../css/styles.css">
 
-    <!--MATERIALIZE-->
-    <link rel="stylesheet" href="../css/materialize/css/materialize.min.css">
+    <!--MATERIALIZE
+    <link rel="stylesheet" href="../css/materialize/css/materialize.min.css"> -->
 
+    <!--FONT AWESOME-->
+
+    <link rel="stylesheet" href="../assets/fontAwesome/fontawesome/css/all.min.css">
+
+    <!-- JQUERY -->
+
+    <script src="https://code.jquery.com/jquery-3.6.0.slim.js" integrity="sha256-HwWONEZrpuoh951cQD1ov2HUK5zA5DwJ1DNUXaM6FsY=" crossorigin="anonymous"></script>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css" />
+
+    <!-- Semantic UI theme -->
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/themes/semantic.min.css" />
+
+    <script src="../js/alertify.js"></script>
 </head>
 
 <body>
 
     <!-- NAV -->
 
-    <nav class="blue darken-4">
+    <nav class="nav" style="background-image: linear-gradient(to right,#4A569D,#DC2424);min-height: 130px;">
         <div class="container ">
             <div class="nav-wrapper ">
-                <a id="logo" href="#" class="brand-logo">Catlovers</a>
+                <a id="logo" href="#" class="brand-logo">Catlovers</a><i class="fas fa-cat fa-5x"></i>
                 <ul id="nav-mobile" class="right hide-on-med-and-down">
-                    <li><a href="../views/index.html">Home</a></li>
+                    <li><a id="options" href="../views/index.html">Home</a></li>
                 </ul>
             </div>
         </div>
@@ -44,20 +64,20 @@
 
     <!-- FORM LOGIN -->
 
-    <h3 class="center">Login</h3>
+    <h3 class="title-form center">Login</h3>
     <div class="row">
         <form action="loginValidate.php" method="post" class="col s12">
             <div class="row">
-                <div class="input-field col s6"><br>
-                    <input id="icon_text" type="text" class="validate" name="username" data-lenght="20">
-                    <label for="icon_text">Username</label>
+                <div class="input-field col s6"><br><br>
+                    <input id="username" type="text" class="validate" name="username" data-lenght="20">
+                    <label id="label" for="icon_text">Username</label>
                 </div>
-                <div class="input-field col s6"><br>
-                    <input id="icon_prefix" type="text" class="validate" name="password" data-lenght="10">
-                    <label for="icon_prefix">Password</label>
+                <div class="input-field col s6"><br><br>
+                    <input id="password" type="text" class="validate" name="password" data-lenght="10">
+                    <label id="label" for="icon_prefix">Password</label>
                 </div>
             </div>
-            <button class="btn waves-effect waves-light" id="send" type="submit" name="submit">Login</button>
+            <button id="send" class="waves-effect waves-light btn-large center-align" type="submit" name="submit">Login</button>
         </form>
     </div>
 
@@ -65,20 +85,24 @@
 
 
 
-    <!-- FOOTER -->
+   <!-- FOOTER -->
 
-    <footer class="footer-copyright blue-darker-4 center">
-        <div class="container">
-            Sergi Sánchez @2021
+   <footer class="page-footer" style="background-image: linear-gradient(to right,#4A569D,#DC2424);">
+        <div class="footer-copyright">
+            <div class="container center">
+                Copyright © 2021 Sergi Sánchez
+            </div>
         </div>
     </footer>
 
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 
-    <!-- Compiled and minified JavaScript 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>-->
+    <!-- <script src="css/materialize/js/materialize.min.js"></script> -->
 
+    <!-- JAVASCRIPT ALERTIFY -->
+    <script src="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/alertify.min.js"></script>
 
-    <script src="css/materialize/js/materialize.min.js"></script>
 
 
     <script>
@@ -86,10 +110,10 @@
             var elems = document.querySelectorAll('.sidenav');
             var instances = M.Sidenav.init(elems);
         });
-
         $(document).ready(function() {
-            $('input#input_text').characterCounter();
+            $('input#username', 'input#password').characterCounter();
         });
+
     </script>
 </body>
 
